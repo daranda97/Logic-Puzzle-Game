0f37d95f14ab314c9af2f376724056fa280c0491
szyzygy <48891060+szyzygy@users.noreply.github.com>
2019-07-02 20:09:10 -0600

    added two script assets

---
+++ Tutorial_carry_over/Assets/Fps_cam_look.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fps_cam_look : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /* camera control method 1
+    Vector2 rotation = Vector2.zero;
+    public float speed = 3;
+    //*/
+
+    //*  camera control method 2
+    Vector2 mouse_look;
+    Vector2 Smoothing;
+    public float mouse_sensitivity = 5f;
+    public float smoothing_factor = 2f;
+    //*/
+
+
+    GameObject character;
+
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        /*
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+        //*/
+
+
+        var target = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        target = Vector2.Scale(target,new Vector2(mouse_sensitivity * smoothing_factor, mouse_sensitivity * smoothing_factor));
+        Smoothing.x = Mathf.Lerp(Smoothing.x, target.x, 1f / smoothing_factor);
+        Smoothing.y = Mathf.Lerp(Smoothing.y, target.y, 1f / smoothing_factor);
+        mouse_look += Smoothing;
+        mouse_look.y = Mathf.Clamp(mouse_look.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouse_look.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouse_look.x, character.transform.up);
+       
+
+
+
+    }
+}
---
+++ Tutorial_carry_over/Assets/player_movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float move_speed = 10.0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float forward = Input.GetAxis("Vertical") * move_speed;
+        float strafing = Input.GetAxis("Horizontal") * move_speed;
+        forward *= Time.deltaTime;
+        strafing *= Time.deltaTime;
+
+        transform.Translate(strafing, 0, forward);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+        
+    }
+}
